12a3b7f178f5831c4c03a0a4cd90d4a8228e2dfc
diogo <lohn.diogo@gmail.com>
2023-03-23 15:17:42 +0000

    Computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+   let num = Math.floor(Math.random() * 3) + 1;
+   if (num === 1){
+    return "Rock";
+   } else if (num === 2) {
+    return "Paper";
+   } else if (num === 3) {
+    return "Scissors";
+   } else {
+    return "Something went wrong";
+   }
+};
+
+console.log(getComputerChoice());
